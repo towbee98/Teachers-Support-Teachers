0d0e8481f9597f1498984631e27ea673a5044b20
Oladele Tobi <tobiemma200@gmail.com>
2021-02-10 11:39:30 +0100

    updated the web page

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 const form = document.forms[0];
 const submitBtn = form[4];
-const result = document.querySelector(".Result");
+const result = document.querySelector('.Result');
 //Collect Contact Form Contents
 
-submitBtn.addEventListener("click", async (event) => {
+submitBtn.addEventListener('click', async (event) => {
   event.preventDefault(); //prevent the button from submitting automatically
   //Store all the input details in an object
   const user = {
@@ -14,38 +14,35 @@ submitBtn.addEventListener("click", async (event) => {
   };
   //Store the object needed for fetching api
   const options = {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(user),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   };
 
-  if (user.name != "" && user.email != "" && user.message != "") {
-    await fetch(
-      "https://teachers-support-teachers.herokuapp.com/email",
-      options
-    )
+  if (user.name != '' && user.email != '' && user.message != '') {
+    await fetch('https://tst-contactme-api.herokuapp.com/email', options)
       .then((res) => {
         if (res.ok) {
-          return Promise.resolve("Message Sent");
+          return Promise.resolve('Message Sent');
         } else {
-          return Promise.reject("Error Occured");
+          return Promise.reject('Error Occured');
         }
       })
       .then((data) => {
         result.innerText = data;
       })
       .catch((err) => {
-        result.innerText = "Internal Server Error";
+        result.innerText = 'Internal Server Error';
       });
     //Clear all inputs field on the user page
     for (let i = 0; i < form.length; i++) {
-      form[i].value = "";
+      form[i].value = '';
     }
     //Clear the result message after a specified period
     setTimeout(function () {
-      result.style.display = "none";
+      result.style.display = 'none';
     }, 5000);
   }
 });
